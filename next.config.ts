24063e37a3e1ae9fde911acc0209eaf692d1f6ca
hacker-c <1507559148@qq.com>
2025-06-05 23:21:38 +0800

    :bug: fix: deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
-// next.config.ts
-import type { NextConfig } from 'next'
+import path from "path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [] // 你的图片配置
-  },
-  output: 'standalone',
-}
 
-export default nextConfig
+  // ✅ Webpack 配置：别名映射到你的 Prisma Client 自定义输出目录
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@/actions/generated/client"] = path.resolve(
+      __dirname,
+      "src/actions/generated/client"
+    );
+
+    if (isServer) {
+      // 解决 Prisma 在 Vercel 找不到 query engine 的问题
+      config.externalsPresets = { node: true };
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
