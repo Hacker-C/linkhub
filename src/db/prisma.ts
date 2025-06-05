f89cf69d72d781aa4801317e68921444bef84506
hacker-c <1507559148@qq.com>
2025-06-05 23:29:07 +0800

    :bug: fix: deploy

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,4 +1,16 @@
 import { PrismaClient } from "@/actions/generated/client";
+import * as fs from "node:fs";
+import path from "path";
+
+if (process.env.NODE_ENV === "production") {
+  // 强制包含这个文件让 output tracing 发现它
+  fs.existsSync(
+    path.resolve(
+      process.cwd(),
+      "src/actions/generated/client/libquery_engine-rhel-openssl-3.0.x.so.node"
+    )
+  );
+}
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
